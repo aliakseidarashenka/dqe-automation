1573d0052250e1f8ffb5839c2a47522e866c3585
aliakseidarashenka <74252139+aliakseidarashenka@users.noreply.github.com>
2026-04-21 12:31:28 +0200

    Update test_facility_name_min_time_spent_per_visit_date.py

--- PyTest DQ Framework/tests/dq checks/parquet_files/test_facility_name_min_time_spent_per_visit_date.py
+++ PyTest DQ Framework/tests/dq checks/parquet_files/test_facility_name_min_time_spent_per_visit_date.py
@@ -1,45 +1,49 @@
 """
 Description: data Quality checks for facility_name_min_time_spent_per_visit_date dataset.
-Requirement(s): TICKET-1234
-Author(s): Name Surname
+Requirement(s): TICKET- 1
+Author(s): Aliaksei Darashenka
 """
 
 import pytest
 import os
 
+pytestmark = [pytest.mark.facility_name_min_time_spent_per_visit_date, pytest.mark.parquet_data]
+
 @pytest.fixture(scope="module")
 def target_data(parquet_reader):
-    path = os.path.join(
-        os.path.dirname(__file__),
-        "../../parquet_data/facility_name_min_time_spent_per_visit_date"
-    )
-    return parquet_reader.process(path)
+    """
+        Load parquet dataset 
+    """
+    return parquet_reader.read("facility_name_min_time_spent_per_visit_date")
 
 
-@pytest.mark.parquet_data
-@pytest.mark.smoke
+# BASIC DQ CHECKS
+
 def test_check_dataset_is_not_empty(target_data, data_quality_library):
+    """
+        Check that parquet is not empty
+    """
     data_quality_library.check_dataset_is_not_empty(target_data)
 
-@pytest.mark.parquet_data
 def test_check_not_null_values(target_data, data_quality_library):
+    """
+        Check that parquet don't contatin null values in the list of column
+    """
     data_quality_library.check_not_null_values(
         target_data,
         column_names=["facility_name", "visit_date", "min_time_spent"])
 
-@pytest.mark.parquet_data
-@pytest.mark.xfail(reason="Need to confirm whether facility_name + visit_date must be unique")
+# @pytest.mark.xfail(reason="Need to confirm whether facility_name + visit_date must be unique")
 def test_check_duplicates(target_data, data_quality_library):
+    """
+        Check uniqness by composite key
+    """
     data_quality_library.check_duplicates(
         target_data,
         column_names=["facility_name", "visit_date", "min_time_spent"]
     )
 
-#@pytest.mark.parquet_data
-#def test_check_expected_row_count(target_data, data_quality_library):
-#    data_quality_library.check_expected_row_count(target_data, expected_count=2)
-
-
+# Business logic check
 @pytest.fixture(scope="module")
 def source_data(db_connection):
     query = """
@@ -58,4 +62,26 @@ def source_data(db_connection):
 
 @pytest.mark.parquet_data
 def test_check_count_source_vs_target(source_data, target_data, data_quality_library):
+    """
+        Row count in parquet and DB should match
+    """
     data_quality_library.check_count(source_data, target_data)
+
+
+def test_parquet_vs_db_data_match(
+    target_data,
+    db_connection,
+    data_quality_library
+):
+    """Parquet data should fully match DB aggregation"""
+
+    source_data = get_avg_visit_data(db_connection)
+
+    data_quality_library.check_data_full_data_set(
+        target_data,
+        source_data,
+        key_columns=["facility_type", "visit_date"],
+        compare_columns=["min_time_spent"]
+    )
+
+
